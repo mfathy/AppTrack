5daa3361f275b3414fa93ef1114ad91794291942
Mohammed Fathy <dev.mfathy@gmail.com>
2018-08-12 17:25:36 +0300

    Adds initial load of applications

--- app/src/main/java/com/mfathy/apptrack/MainActivity.java
+++ app/src/main/java/com/mfathy/apptrack/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mfathy.apptrack;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getFragmentManager();
+        // Create the list fragment and add it as our sole content.
+        if (fm.findFragmentById(R.id.fragmentLayout) == null) {
+            AppListFragment list = new AppListFragment();
+            fm.beginTransaction().add(R.id.fragmentLayout, list).commit();
+        }
     }
 }
